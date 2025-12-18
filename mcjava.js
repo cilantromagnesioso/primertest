7022b8c2becbec42190069a6ee1201dbc746f49a
cilantromagnesioso <ramc81735@gmail.com>
2025-12-18 11:17:11 -0700

    la javascript update

---
+++ mcjava.js
@@ -0,0 +1,5 @@
+const unacosa = "Hola otra vez, parece ser que escribiste ";
+const coso = document.getElementById (nowe);
+const temp = ", (esta funcion no esta terminada por eso dice null)"
+
+document.write(unacosa + coso + temp);
