f2bbd125b29695f6230d3025d2fe6a7e41bafdf5
cilantromagnesioso <ramc81735@gmail.com>
2025-12-21 22:04:57 -0700

    1.1.0
    cambios:
    -el cuadro de texto ya funciona
    -la primera seccion esta centrada
    -todas las fuentes de letra han cambiado
    - se añadio la seccion de python (pero sin nada interesante de momento)
    -cambios tecnicos con CSS
    -y alguno que otro cambio menor

--- mcjava.js
+++ mcjava.js
@@ -1,5 +1,6 @@
-const unacosa = "Hola otra vez, parece ser que escribiste ";
-const coso = document.getElementById (nowe);
-const temp = ", (esta funcion no esta terminada por eso dice null)";
-
-document.write(unacosa + coso + temp);
+const unacosa = "Hola otra vez, parece ser que escribiste: ";
+const temp = ", (esta funcion no esta terminada por eso no sale nada)";
+function laburo() {
+    const antilaburo = document.getElementById("nowe").value;
+    document.getElementById("siwe").textContent = (unacosa + antilaburo);
+}
