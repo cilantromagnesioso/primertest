acd7772b0599f003441f7e3e3a9f486951d469ff
cilantromagnesioso <ramc81735@gmail.com>
2026-05-18 09:57:25 -0700

    1.7.1
    cambios:
    - Se añadieron nuevos ejercicios a la seccion del sotano de la prepa
    - El juego del boton saldra pronto
    - Cambios menores

--- escuela_prg.js
+++ escuela_prg.js
@@ -4,7 +4,7 @@ selector.addEventListener("change",function() {
     const selector_valor = this.value;
     if (selector_valor == "z") {
         document.getElementById("ejercicio_s").innerHTML = `<h1>Sotano de la prepa</h1>
-            <p>En esta seccion estaran mis ejercicios de la escuela</p>`
+            <p>En esta seccion estaran mis ejercicios de la escuela, usa el selector de arriba para ver los ejercicios</p>`;
     }
     if (selector_valor == "a") {
         document.getElementById("ejercicio_s").innerHTML = `<h1>Hola salon de programacion</h1>
@@ -101,6 +101,217 @@ selector.addEventListener("change",function() {
             <br><br>
 
             <input type="submit" value="Enviar informacion">
-        </form>`
+        </form>`;
     }
+    if (selector_valor == "d") {
+        document.getElementById("ejercicio_s").innerHTML = `<img src="skul_a.jpg" alt="imagen de escuela" width="50%" height="50%">
+        <h1>Formulario de alumno</h1>
+        <form>
+            <img src="skul_c.png" alt="alumno" width="3%" height="3%">
+            <label>Nombre del alumno:</label>
+            <input type="text" name="nombreestudiante">
+            <br><br>
+
+            <label>Apellido 1:</label>
+            <input type="text" name="apellido1">
+            <br><br>
+
+            <label>Apellido 2:</label>
+            <input type="text" name="apellido2">
+            <br><br>
+
+            <label>Fecha de nacimiento:</label>
+            <input type="date" name="nacimiento">
+            <br><br>
+
+            <label>Calle:</label>
+            <input type="text" name="calledom">
+            <br><br>
+
+            <label>Colonia:</label>
+            <input type="text" name="coloniadom">
+            <br><br>
+
+            <label>Num. Exterior:</label>
+            <input type="number" name="exteriordom">
+            <br><br>
+
+            <label>Tipo de sangre:</label>
+            <img src="skul_b.jpg" alt="tipo de sangre" width="5%" height="5%">
+            <select name="sangretip">
+                <option value="">seleccione</option>
+                <option value="1">A+</option>
+                <option value="2">A-</option>
+                <option value="3">B+</option>
+                <option value="4">B-</option>
+                <option value="5">AB+</option>
+                <option value="6">AB-</option>
+                <option value="7">O+</option>
+                <option value="8">O-</option>
+            </select>
+            <br><br>
+
+            <input type="submit" value="Enviar informacion">
+        </form>`;
+    }
+    if (selector_valor == "e") {
+        document.getElementById("ejercicio_s").innerHTML = `<img src="skul_a.jpg" alt="imagen de escuela" width="50%" height="50%">
+        <hr>
+        <h1>Formulario de alumno</h1>
+        <form>
+            <img src="skul_c.png" alt="alumno" width="3%" height="3%">
+            <label>Nombre del alumno:</label>
+            <input type="text" name="nombreestudiante">
+            <br><br>
+
+            <label>Estudiante regular:</label>
+            <input type="checkbox" name="regular">
+            <br><br>
+            
+            <label>Edad del estudiante:</label>
+            <input type="number" name="edad">
+            <br><br>
+
+            <label>Fecha de nacimiento:</label>
+            <input type="date" name="nacimiento">
+            <br><br>
+
+            <label>Tipo de sangre:</label>
+            <img src="skul_b.jpg" alt="tipo de sangre" width="5%" height="5%">
+            <select name="sangretip">
+                <option value="">seleccione</option>
+                <option value="1">A+</option>
+                <option value="2">A-</option>
+                <option value="3">B+</option>
+                <option value="4">B-</option>
+                <option value="5">AB+</option>
+                <option value="6">AB-</option>
+                <option value="7">O+</option>
+                <option value="8">O-</option>
+            </select>
+            <br><br>
+            
+            <label>Turno:</label>
+            <input type="radio" name="turno" value="m"> Matutino
+            <input type="radio" name="turno" value="v"> Vespertino
+            <input type="radio" name="turno" value="n"> Nocturno
+            <br><br>
+
+            <label>Genero:</label>
+            <input type="radio" name="genero" value="masculino"> Masculino
+            <input type="radio" name="genero" value="femenino"> Femenino
+            <br><br>
+
+            <input type="submit" value="Enviar informacion">
+        </form>`;
+    }
+    if (selector_valor == "f") {
+        document.getElementById("ejercicio_s").innerHTML = `<h1>Informacion de empleado</h1>
+        <form>
+            <label>Nombres:</label>
+            <input type="text" name="nombre_dom">
+            <br><br>
+            <label>Apellidos:</label>
+            <input type="text" name="apelli_dom">
+            <br><br>
+            <label>Fecha de nacimiento:</label>
+            <input type="date" name="fecha_s">
+            <br><br>
+            <label>Telefono:</label>
+            <input type="tel" name="telefon_s">
+            <br><br>
+            <label>Correo:</label>
+            <input type="text" name="Correo_dom">
+            <br><br>
+            <label>Calle:</label>
+            <input type="text" name="calle_dom">
+            <br><br>
+            <label>Numero exterior:</label>
+            <input type="number" name="num_ex">
+            <br><br>
+            <label>Colonia:</label>
+            <input type="text" name="col_dom">
+            <br><br>
+            <label>Codigo postal:</label>
+            <input type="number" name="cod_dom">
+            <br><br>
+            <label>Genero:</label>
+            <input type="radio" name="genero" value="masculino"> Masculino
+            <input type="radio" name="genero" value="femenino"> Femenino
+            <br><br>
+            <label>Empleado activo:</label>
+            <input type="checkbox" name="muri_S">
+            <br><br>
+            <input type="submit" value="Enviar informacion">
+        </form>`;
+    } 
+    if (selector_valor == "e") {
+        document.getElementById("ejercicio_s").innerHTML = `<img src="skul_a.jpg" alt="imagen de escuela" width="50%" height="50%">
+        <hr>
+        <h1>Formulario de alumno</h1>
+        <form>
+            <img src="skul_c.png" alt="alumno" width="3%" height="3%">
+            <label>Nombre del alumno:</label>
+            <input type="text" name="nombreestudiante">
+            <br><br>
+
+            <label>Estudiante regular:</label>
+            <input type="checkbox" name="regular">
+            <br><br>
+            
+            <label>Edad del estudiante:</label>
+            <input type="number" name="edad">
+            <br><br>
+
+            <label>Fecha de nacimiento:</label>
+            <input type="date" name="nacimiento">
+            <br><br>
+
+            <label>Tipo de sangre:</label>
+            <img src="skul_b.jpg" alt="tipo de sangre" width="5%" height="5%">
+            <select name="sangretip">
+                <option value="">seleccione</option>
+                <option value="1">A+</option>
+                <option value="2">A-</option>
+                <option value="3">B+</option>
+                <option value="4">B-</option>
+                <option value="5">AB+</option>
+                <option value="6">AB-</option>
+                <option value="7">O+</option>
+                <option value="8">O-</option>
+            </select>
+            <br><br>
+            
+            <label>Turno:</label>
+            <input type="radio" name="turno" value="m"> Matutino
+            <input type="radio" name="turno" value="v"> Vespertino
+            <input type="radio" name="turno" value="n"> Nocturno
+            <br><br>
+
+            <label>Genero:</label>
+            <input type="radio" name="genero" value="masculino"> Masculino
+            <input type="radio" name="genero" value="femenino"> Femenino
+            <br><br>
+
+            <input type="submit" value="Enviar informacion">
+        </form>`;
+    }
+    if (selector_valor == "g") {
+        document.getElementById("ejercicio_s").innerHTML = `<h1>Durango</h1>
+        <h2>Caldillo duranguense</h2>
+        <img src="skul_d.jpg" class="img1skul">
+        <p>El caldillo duranguense es un emblemático platillo del norte de México, originario del estado de Durango. Consiste en un caldo sustancioso hecho a base de carne de res (fresca o seca), jitomate, cebolla, ajo, comino y el toque característico de chiles poblanos o "chiles pasados" (chiles chilacas secos).</p>
+        <br>
+        <h2>Flora y fauna</h2>
+        <img src="skul_e.jpg" class="img1skul">
+        <p>- Zonas áridas y semidesérticas: Abundan especies xerófilas como nopales, agaves (sotol), matorrales espinosos y diversos tipos de cactus.</p>
+        <p>- Mamíferos: Oso negro (cuya población se ha recuperado en años recientes), lobo mexicano, puma, lince rojo, venado cola blanca, coyote, coatí y mapache.</p>
+        <p>- Aves: Águila real, águila pescadora y cabeza blanca, guajolote salvaje, halcón peregrino, y el pato tepalcate.</p>
+        <br>
+        <h2>Plaza de armas</h2>
+        <img src="skul_f.jpg" class="img1skul">
+        <p>La Plaza de Armas es el epicentro social y cultural del Centro Histórico de Durango. Ubicada frente a la Catedral Basílica Menor, funciona como un punto de reunión tradicional donde locales y visitantes disfrutan de la música en vivo, el descanso bajo sus árboles y la arquitectura colonial de la zona.</p>
+        <p> Es muy común encontrar músicos y tríos tocando serenatas tradicionales al caer la tarde, además de una amplia oferta de cafeterías y restaurantes en sus alrededores.</p>
+        <br>`;
+    } 
 })
